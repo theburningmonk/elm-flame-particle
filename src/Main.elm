93cea77b98ad8e75dbac33b55046832036c44e18
theburningmonk@gmail.com <theburningmonk@gmail.com>
2015-03-13 14:15:33 +0000

    - small improvement

--- src/Main.elm
+++ src/Main.elm
@@ -13,13 +13,13 @@ import Debug
 ticks : Signal Float
 ticks = Signal.foldp (\_ n -> n + 1) 0 (Time.fps 10)
 
-type alias Particle = {x:Float, y:Float, radius:Float, color:Color, seed:Float}
+type alias Particle = {x:Float, y:Float, radius:Float, color:Color, seed:Float, t:Int}
 type alias State = {particles:List Particle, seed:Random.Seed}
 
 defaultState = {particles=[], seed=Random.initialSeed 42}
 
 step : Float -> State -> State
-step t state = state |> prune |> update t |> spawn 15
+step t state = state |> prune |> update |> spawn t
 
 state : Signal State
 state = Signal.foldp step defaultState ticks
@@ -44,33 +44,36 @@ prune state =
   let newParticles = 
     state.particles |> List.filter (\p -> 
       let {red, green, blue, alpha} = toRgb p.color
-      in alpha > 0)
+      in alpha > 0 && p.radius > 0)
   in {state | particles<-newParticles}
 
-update : Float -> State -> State
-update t state = 
+update : State -> State
+update state = 
   let newParticles =
     state.particles |> List.map (\p -> 
       let {red, green, blue, alpha} = toRgb p.color
-          newR = red   + (sin (t * p.seed) |> (*) 30 |> floor)
-          newG = green + (sin (t * p.seed) |> (*) 15 |> floor)
-          newB = blue --  + (sin (t * p.seed) |> (*) 15 |> floor)
-          newA = (alpha - 0.014 * p.seed) * 0.98
+          newR = red   + (sin (toFloat p.t * p.seed) |> (*) 40 |> floor)
+          newG = green + (sin (toFloat p.t * p.seed) |> (*) 33 |> floor)
+          newB = blue  + (sin (toFloat p.t * p.seed) |> (*) 15 |> floor)
+          newA = (alpha - 0.014 * p.seed) * 0.97
           newColor = rgba newR newG newB newA
-      in {p | x <- p.x - sin (t * 0.386) * p.seed,
-              y <- p.y + 1.5,
+      in {p | x <- p.x - sin (toFloat p.t * 0.386),
+              y <- p.y + 1,
               radius <- p.radius - 0.03,
-              color  <- newColor})
+              color  <- newColor,
+              t <- p.t + 1})
   in {state | particles<-newParticles}
 
-spawn : Int -> State -> State
-spawn n state =
-    case n of
-        0 -> state
-        n -> 
-          let (newParticle, newSeed) = makeParticle state.seed
-          in spawn (n-1) {state | particles<-newParticle::state.particles
-                                , seed     <-newSeed}
+spawn : Float -> State -> State
+spawn t state =
+    --case n of
+    --    0 -> 
+          let newParticles = List.append (floor t |> genParticles) state.particles
+          in {state | particles<-newParticles}
+    --    n -> 
+    --      let (newParticle, newSeed) = makeParticle state.seed
+    --      in spawn 0 {state | particles<-newParticle::state.particles
+    --                        , seed     <-newSeed}
 
 makeParticle : Random.Seed -> (Particle, Random.Seed)
 makeParticle seed =
@@ -85,5 +88,19 @@ makeParticle seed =
 
         particle = { x=x, y=y, radius=5,
                      color=rgba r g b alpha,
-                     seed=pSeed }
+                     seed=pSeed,
+                     t=42 }
     in (particle, seed8)
+
+genParticles : Int -> List Particle
+genParticles t =
+    [
+      {x=-3,  y=2,  radius=5, color=rgba 255 40 10 0.86,  seed=0.5, t=9},
+      {x=2,   y=2,  radius=5, color=rgba 155 73 10 0.86,  seed=0.5, t=4},
+      {x=-1,  y=1,  radius=5, color=rgba 255 128 51 0.86, seed=(toFloat t |> sin) * 0.9, t=t%10},
+      {x=-2,  y=0,  radius=5, color=rgba 111 40 40 0.74,  seed=0.714, t=9},
+      {x=1,   y=0,  radius=5, color=rgba 170 83 32 0.86,  seed=0.4 + (toFloat t |> sin) * 0.362, t=t%3},
+      {x=0.7, y=1,  radius=5, color=rgba 168 83 5 0.98,  seed=0.4, t=t%4},
+      {x=1,   y=0,  radius=5, color=rgba 177 72 17 0.85, seed=1.154, t=0},
+      {x=-0.5, y=1, radius=5, color=rgba 189 20 17 0.85, seed=0.6, t=3}
+    ]
